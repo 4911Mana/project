8bc18277c0479823133ee8ca48bdfce2eaab1d05
anna <bershika@gmail.com>
2011-04-10 16:27:46 -0700

    exception added

---
+++ src/main/java/comp/is/controller/project/DataInconsistencyException.java
@@ -0,0 +1,10 @@
+package comp.is.controller.project;
+
+public class DataInconsistencyException extends Exception {
+
+    public DataInconsistencyException(String message) {
+        super(message);
+        
+    }
+
+}
